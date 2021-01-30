3bc626ef990c1a6c2ac639a77e8aff8afb51f88a
admin <d.gerasimenko@trace-it.ru>
2021-01-30 10:34:38 +0300

    001

---
+++ DysonCustomerService/DysonCustomerService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DysonCustomerService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ObmenCreation();
+
+            service.Url = @"http://31.13.35.34/dyson_share_111/ws/ObmenCreation.1cws";
+
+            service.Credentials = new NetworkCredential("yandex", "a123");
+
+            service.PostTovars(new ПакетНоменклатуры()
+            {
+                
+            });
+        }
+    }
+}
